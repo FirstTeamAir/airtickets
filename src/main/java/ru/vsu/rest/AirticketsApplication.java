33201e77657c350a2644afa685879282c80dc824
Vlad Kolesnikov <vkoles2611@gmail.com>
2021-04-07 21:45:29 +0300

    Added Spring application

---
+++ src/main/java/ru/vsu/rest/AirticketsApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirticketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/rest/AirticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
